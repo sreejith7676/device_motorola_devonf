f06756f63d983f853f397e060ea42d02f4f9f797
Sarthak Roy <sarthakroy2002@gmail.com>
2024-12-09 19:11:21 +0530

    devonf: Build android.hardware.sensors@2.1-service.devonf-multihal
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I4285f710baa3848fc41624402021e3cd1efbe9ef
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -433,7 +433,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors@2.1-service.devonf-multihal
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
