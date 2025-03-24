7b0ca7f782af20c1e7d78aebdf26c282e478a63c
Michael Bestas <mkbestas@lineageos.org>
2025-03-24 02:45:07 +0000

    devonf: Shim few MTK libs with libbase_shim
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -75,6 +75,10 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('1.1', '1.2')
         .regex_replace('@1.0', '@1.2')
         .regex_replace('default9', 'default'),
+    ('vendor/lib/mt6855/libneuralnetworks_sl_driver_mtk_prebuilt.so', 'vendor/lib64/mt6855/libneuralnetworks_sl_driver_mtk_prebuilt.so', 
+     'vendor/lib64/libstfactory-vendor.so', 'vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 
+     'vendor/lib/libsysenv.so', 'vendor/lib64/libsysenv.so', 'vendor/lib/libtflite_mtk.so', 'vendor/lib64/libtflite_mtk.so'): blob_fixup()
+        .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
