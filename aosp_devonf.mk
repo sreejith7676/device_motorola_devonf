2074fd93aae598bed72940631a04834173797b91
sreejith7676 <sreejithsss014@gmail.com>
2025-06-28 21:40:19 +0530

    devonf: Update to U1TNS34.82-12-7-9

--- aosp_devonf.mk
+++ aosp_devonf.mk
@@ -25,5 +25,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=devonf \
-    BuildDesc="devonf_g_sys-user 14 U1TNS34M.82-12-7-4 00d08 release-keys" \
-    BuildFingerprint=motorola/devonf_g_sys/devonf:14/U1TNS34M.82-12-7-4/00d08:user/release-keys
+    BuildDesc="devonf_g_sys-user 14 U1TNS34M.82-12-7-9 4db9ca release-keys" \
+    BuildFingerprint=motorola/devonf_g_sys/devonf:14/U1TNS34M.82-12-7-9/4db9ca:user/release-keys
