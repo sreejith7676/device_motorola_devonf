67b39b3b592abe628029e89df20b334cea3d21fd
Adarsh Singh <singh.adarsh@aol.com>
2025-07-14 09:15:41 +0530

    devonf: Adapt to Lineage
    Signed-off-by: Adarsh Singh <singh.adarsh@aol.com>

--- lineage_devonf.mk
+++ lineage_devonf.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/motorola/devonf/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_devonf
+PRODUCT_NAME := lineage_devonf
 PRODUCT_DEVICE := devonf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
