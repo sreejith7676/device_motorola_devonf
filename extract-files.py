fe83d344e32cadc10a57561526b836db0afc9488
Mashopy <eliasgheeraert@gmail.com>
2025-07-14 11:11:36 +0530

    devonf: Format blob_fixups

--- extract-files.py
+++ extract-files.py
@@ -48,13 +48,24 @@ blob_fixups: blob_fixups_user_type = {
         .apktool_patch('ims-patches'),
     'system_ext/priv-app/MtkGbaService/MtkGbaService.apk': blob_fixup()
         .apktool_patch('MtkGba-patches'),
-    ('system_ext/etc/init/init.vtservice.rc', 'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc'): blob_fixup()
+    (
+        'system_ext/lib/libarmnn_ndk.mtk.so',
+        'system_ext/lib64/libarmnn_ndk.mtk.so'
+    ): blob_fixup()
+        .add_needed('liblog.so'),
+    (
+        'system_ext/etc/init/init.vtservice.rc',
+        'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc'
+    ): blob_fixup()
         .regex_replace('start', 'enable'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
-    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+    (
+        'vendor/bin/hw/android.hardware.gnss-service.mediatek',
+        'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'
+    ): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
-    ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek','vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
     'vendor/bin/hw/android.hardware.security.keymint-service.trustonic': blob_fixup()
@@ -62,39 +73,67 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so'),
-    ('vendor/bin/mnld', 'vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so', 'vendor/lib64/mt6855/libcam.utils.sensorprovider.so'): blob_fixup()
+    (
+        'vendor/bin/mnld',
+        'vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so',
+        'vendor/lib64/mt6855/libcam.utils.sensorprovider.so'
+    ): blob_fixup()
         .add_needed('android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/mt6855/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
         .add_needed('android.hardware.sensors@1.0-convert-shared.so')
         .replace_needed('libutils.so', 'libutils-v32.so')
-        .replace_needed('libtinyxml2.so', 'libtinyxml2-bp2a.so'),
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-legacy.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libutils.so','libutils-v32.so')
         .replace_needed('libalsautils.so','libalsautils-v31.so')
-        .replace_needed('libtinyxml2.so', 'libtinyxml2-bp2a.so'),
-    ('vendor/lib64/hw/mt6855/android.hardware.camera.provider@2.6-impl-mediatek.so','vendor/lib64/mt6855/libmtkcam_stdutils.so',
-     'vendor/lib64/sensors.moto.so'): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-legacy.so'),
+    (
+        'vendor/lib64/hw/mt6855/android.hardware.camera.provider@2.6-impl-mediatek.so',
+        'vendor/lib64/mt6855/libmtkcam_stdutils.so',
+        'vendor/lib64/sensors.moto.so'
+    ): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so')
-         .add_needed('libbase_shim.so'),
-    ('vendor/lib64/mt6855/lib3a.flash.so', 'vendor/lib64/mt6855/lib3a.ae.stat.so', 
-     'vendor/lib64/mt6855/lib3a.sensors.flicker.so', 'vendor/lib64/mt6855/lib3a.sensors.color.so', 
-     'vendor/lib64/mt6855/libaaa_ltm.so', 'vendor/lib64/lib3a.ae.pipe.so'): blob_fixup()
+        .add_needed('libbase_shim.so'),
+    (
+        'vendor/lib64/mt6855/lib3a.flash.so',
+        'vendor/lib64/mt6855/lib3a.ae.stat.so',
+        'vendor/lib64/mt6855/lib3a.sensors.flicker.so',
+        'vendor/lib64/mt6855/lib3a.sensors.color.so',
+        'vendor/lib64/mt6855/libaaa_ltm.so',
+        'vendor/lib64/lib3a.ae.pipe.so'
+    ): blob_fixup()
         .add_needed('liblog.so'),
     'vendor/lib64/mt6855/libmnl.so': blob_fixup()
         .add_needed('libcutils.so'),
-    ('vendor/lib64/libdlbdsservice.so', 'vendor/lib64/libcodec2_soft_ddpdec.so',
-     'vendor/lib64/soundfx/libswdap.so','vendor/lib64/soundfx/libdlbvol.so',
-     'vendor/lib64/libcodec2_soft_ac4dec.so','vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service'): blob_fixup()
+    (
+        'vendor/lib64/libdlbdsservice.so',
+        'vendor/lib64/libcodec2_soft_ddpdec.so',
+        'vendor/lib64/soundfx/libswdap.so',
+        'vendor/lib64/soundfx/libdlbvol.so',
+        'vendor/lib64/libcodec2_soft_ac4dec.so',
+        'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service'
+    ): blob_fixup()
         .add_needed('libstagefright_foundation-v33.so'),
     'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
         .regex_replace('1.1', '1.2'),
-    ('vendor/lib/mt6855/libneuralnetworks_sl_driver_mtk_prebuilt.so', 'vendor/lib64/mt6855/libneuralnetworks_sl_driver_mtk_prebuilt.so', 
-     'vendor/lib64/libstfactory-vendor.so', 'vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 
-     'vendor/lib/libsysenv.so', 'vendor/lib64/libsysenv.so', 'vendor/lib/libtflite_mtk.so', 'vendor/lib64/libtflite_mtk.so'): blob_fixup()
+    (
+        'vendor/lib/mt6855/libneuralnetworks_sl_driver_mtk_prebuilt.so',
+        'vendor/lib64/mt6855/libneuralnetworks_sl_driver_mtk_prebuilt.so',
+        'vendor/lib64/libstfactory-vendor.so',
+        'vendor/lib/libnvram.so',
+        'vendor/lib64/libnvram.so',
+        'vendor/lib/libsysenv.so',
+        'vendor/lib64/libsysenv.so',
+        'vendor/lib/libtflite_mtk.so',
+        'vendor/lib64/libtflite_mtk.so'
+    ): blob_fixup()
         .add_needed('libbase_shim.so'),
-    ('vendor/lib64/mt6855/libcam.hal3a.v3.so', 'vendor/lib64/hw/hwcomposer.mtk_common.so'): blob_fixup()
-         .add_needed('libprocessgroup_shim.so'),
+    (
+        'vendor/lib64/mt6855/libcam.hal3a.v3.so',
+        'vendor/lib64/hw/hwcomposer.mtk_common.so'
+    ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
     'vendor/bin/hw/mtkfusionrild': blob_fixup()
         .add_needed('libutils-v32.so'),
     'system_ext/lib64/libimsma.so': blob_fixup()
