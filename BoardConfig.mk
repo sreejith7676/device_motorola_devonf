595c961cafc9c4baed31541692e1bcfe184b9607
Mashopy <eliasgheeraert@gmail.com>
2025-08-09 08:41:17 +0530

    devonf: Update manifest/device compatibility matrix to v8.0 format
    * Move c2 manifest to proprietary-files.txt
      and use patch it using extraction
    
    Change-Id: Ie036f9a5ed51d2b1afb4d42e828fa7d73edd4095

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/mot_aids.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/device_framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
+    
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
