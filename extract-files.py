0637a1980afed82433775e9fcd4cd7d6cabcd70a
Mashopy <eliasgheeraert@gmail.com>
2025-12-20 15:01:25 +0530

    devonf: Patch audio and PQ blobs with libtinyxml2-bp2a from A16 qpr0
    Google broke ABI for tinyXML2 in Android 16 QPR2, causing fatal crashes as per logcats.

--- extract-files.py
+++ extract-files.py
@@ -59,11 +59,13 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/mt6855/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
         .add_needed('android.hardware.sensors@1.0-convert-shared.so')
-        .replace_needed('libutils.so', 'libutils-v32.so'),
+        .replace_needed('libutils.so', 'libutils-v32.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-bp2a.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libutils.so','libutils-v32.so')
-        .replace_needed('libalsautils.so','libalsautils-v31.so'),
+        .replace_needed('libalsautils.so','libalsautils-v31.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-bp2a.so'),
     ('vendor/lib64/hw/mt6855/android.hardware.camera.provider@2.6-impl-mediatek.so','vendor/lib64/mt6855/libmtkcam_stdutils.so',
      'vendor/lib64/sensors.moto.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so')
