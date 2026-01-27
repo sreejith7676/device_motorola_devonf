975c30dffd51cb7f940481091c862641b3d8a791
sreejith7676 <sreejithsss014@gmail.com>
2026-01-27 12:48:28 +0530

    devonf: Drop `OpenDelta`
    * Not needed for clover
    
    Signed-off-by: sreejith7676 <sreejithsss014@gmail.com>

--- device.mk
+++ device.mk
@@ -254,7 +254,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayDevonf \
     FrameworksResOverlayDevonf \
-    OpenDeltaOverlayDevonf \
     PowerOffAlarmOverlayDevonf \
     SettingsOverlayDevonf \
     SettingsProviderOverlayDevonf \
