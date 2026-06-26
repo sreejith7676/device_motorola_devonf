06ce7d9650f35d6c81f6473e0095e9257dd2c1f7
sreejith7676 <sreejithsss014@gmail.com>
2026-06-26 11:13:10 +0530

    avium

--- avium_devonf.mk
+++ avium_devonf.mk
@@ -12,18 +12,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/devonf/device.mk)
 
 # Inherit some common YAAP stuff.
-$(call inherit-product, vendor/clover/config/common_full_phone.mk)
+$(call inherit-product, vendor/avium/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := clover_devonf
+PRODUCT_NAME := avium_devonf
 PRODUCT_DEVICE := devonf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g73 5G 
 
-CLOVER_MAINTAINER := Sreejith
-TARGET_ENABLE_BLUR := true
-
+AVIUM_SETTINGS_SOC_MODEL_NAME := Dimensity 
+AVIUM_SETTINGS_DEVICE_CODENAME := devonf
+WITH_GMS := ture
+TARGET_FORCE_ENABLE_BLUR := true
+AVIUM_FORCE_SET_FAKE_PROP := true
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
